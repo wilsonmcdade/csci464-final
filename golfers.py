57db06f6f8f3f2443c2b78e2615daa68ba03f64e
kmw1704 <kmw1704@g.rit.edu>
2024-12-06 14:38:46 -0500

    team class and print function

--- golfers.py
+++ golfers.py
@@ -10,10 +10,44 @@ class Person:
         # where X = 1 means (negative) preference
 
 
+class Team:
+    def __init__(self, teamnum, members, unitnum):
+        self.teamnum = teamnum
+        self.members = members # list of people
+        self.unitnum = unitnum
+
+    def get_teamstr(self): # to help with printing
+        finalstr = "Team " + str(self.teamnum) + " ["
+        first = True
+        for member in self.members:
+            if first:
+                finalstr = finalstr + member.name
+                first = False
+            else:
+                finalstr = finalstr + ", " + member.name
+        finalstr = finalstr + "]"
+        return finalstr
+
+
+def make_team(teamnum, people, unitnum):
+    newteam = Team(teamnum, people, unitnum)
+    for personi in range(len(people)):
+        without_person = people.copy()
+        without_person.pop(personi)
+        for notperson in without_person:
+            people[personi].partners.append(notperson)
+    # that should update all the people
+    return newteam
+
+
+
 def main():
     # do main things
     person1 = Person("person1", 0, [], [])
-    print(person1)
+    person2 = Person("person2", 1, [], [])
+    person3 = Person("person3", 2, [], [])
+    team1 = make_team(1, [person1, person2,  person3], 1)
+    print(team1.get_teamstr())
 
 
 if __name__ == '__main__':
