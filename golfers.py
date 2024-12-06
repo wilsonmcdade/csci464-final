5c56e93ec080acdb5470396397d1fd34f903b2df
kmw1704 <kmw1704@g.rit.edu>
2024-12-06 14:15:57 -0500

    added attr to class

--- golfers.py
+++ golfers.py
@@ -1,7 +1,20 @@
 
 
-class Config:
-    def __init__(self, name, id, partners):
+class Person:
+    def __init__(self, name, id, partners, preferences):
         self.name = name
         self.id = id
         self.partners = partners
+        self.preference = preferences # list of tuples (name of undesired partener, X)
+        # where X = 0 means absolute rule
+        # where X = 1 means (negative) preference
+
+
+def main():
+    # do main things
+    person1 = Person("person1", 0, [], [])
+    print(person1)
+
+
+if __name__ == '__main__':
+    main()
