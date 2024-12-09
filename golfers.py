c0767d9c55f3c79bae2b1bbe24dd4ae801e57980
Lilian Ready <dezingerlily@gmail.com>
2024-12-09 10:29:06 -0500

    creates the first set of teams for round robin

--- golfers.py
+++ golfers.py
@@ -1,4 +1,4 @@
-
+from operator import itemgetter
 
 class Person:
     def __init__(self, name, id, partners, preferences):
@@ -40,14 +40,54 @@ def make_team(teamnum, people, unitnum):
     return newteam
 
 
+def round_robin(teamcount, all_people,teamsize):
+    # initial setup
+    # 4 x count grid, each column is a team
+    team_grid = [[0 for x in range(teamsize)] for y in range(teamcount)]
+    people_ind = 0
+    round = 0
+    all_teams = []
+
+    # Round 1
+    for j in range(teamsize):
+        for i in range(teamcount):
+            if people_ind < len(all_people):
+                team_grid[i][j] = all_people[people_ind]
+                people_ind = people_ind + 1
+
+    for i in range(teamcount):
+        team = make_team(i,team_grid[i],round)
+        all_teams.append(team)
+
+    # From here we need to shift everything except 1 to the right until the teams come out the same
+
+    return all_teams
+
+    
 
 def main():
     # do main things
     person1 = Person("person1", 0, [], [])
     person2 = Person("person2", 1, [], [])
     person3 = Person("person3", 2, [], [])
-    team1 = make_team(1, [person1, person2,  person3], 1)
+    person4 = Person("person4", 0, [], [])
+    person5 = Person("person5", 1, [], [])
+    person6 = Person("person6", 2, [], [])
+    person7 = Person("person7", 0, [], [])
+    person8 = Person("person8", 1, [], [])
+    person9 = Person("person9", 2, [], [])
+    person10 = Person("person10", 0, [], [])
+    person11 = Person("person11", 1, [], [])
+    person12 = Person("person12", 2, [], [])
+    person13 = Person("person13", 1, [], [])
+    person14 = Person("person14", 2, [], [])
+
+    all_people = [person1,person2,person3,person4,person5,person6,person7,person8,person9,person10,person11,person12,person13,person14]
+
+    team1 = make_team(1, [person1, person2, person3], 1)
     print(team1.get_teamstr())
+
+    round_robin(7,all_people,2)
 
 
 if __name__ == '__main__':
