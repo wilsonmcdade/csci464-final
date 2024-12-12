a34d9a1e8a225abd8abb5cfd2d945543f66433d9
Wilson McDade <wilson.mcdade@gmail.com>
2024-12-11 23:35:33 -0500

    Added eval function, print function for persons

--- golfers.py
+++ golfers.py
@@ -9,6 +9,28 @@ class Person:
         # where X = 0 means absolute rule
         # where X = 1 means (negative) preference
 
+    def get_personstr(self):
+        finalstr = "Person: " + self.name + ", ID: " + str(self.id) + ",\t\t Partners: ["
+        first = True
+        for partner in self.partners:
+            if first:
+                finalstr = finalstr + partner.name
+                first = False
+            else:
+                finalstr = finalstr + ", " + partner.name
+        finalstr = finalstr + "], Preferences: ["
+
+        first = True
+        for pref in self.preference:
+            if first:
+                finalstr = finalstr + pref[0] + str(pref[1])
+                first = False
+            else:
+                finalstr = finalstr + ", " + pref[0] + str(pref[1])
+        finalstr = finalstr + "]"
+        
+        return finalstr
+
 
 class Team:
     def __init__(self, teamnum, members, unitnum):
@@ -39,7 +61,27 @@ def make_team(teamnum, people, unitnum):
     # that should update all the people
     return newteam
 
+"""
+Evaluate goodness of team pairings 
+Looks at previous partnerships and rule breakage
+TODO: Add a smarter grading algorithm to handle some of the constraints we've chosen
+"""
+def evaluate(teams):
+    scores = list()
+    for team in teams:
+        badness = 0
+        for person in team.members:
+            for pref in person.preference:
+                if pref[0] in team.members:
+                    badness += pref[1]
+            for partner in person.partners:
+                if partner in team.members:
+                    badness += 1
+                badness -= team.unitnum
 
+        scores.append(badness)
+
+    return scores
 
 def main():
     # do main things
@@ -47,6 +89,9 @@ def main():
     person2 = Person("person2", 1, [], [])
     person3 = Person("person3", 2, [], [])
     team1 = make_team(1, [person1, person2,  person3], 1)
+    print(evaluate([team1]))
+    for m in team1.members:
+        print(m.get_personstr())
     print(team1.get_teamstr())
 
 
