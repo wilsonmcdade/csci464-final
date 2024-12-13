8ca985a1a0a2bdddfadb5e967ceca9da850390ed
Lilian Ready <dezingerlily@gmail.com>
2024-12-13 12:01:49 -0500

    final push :)

--- grouping.py
+++ grouping.py
@@ -1,7 +1,6 @@
 import copy
 import math
 import random
-from itertools import combinations
 
 class Person:
     def __init__(self, name, id, partners, preferences):
@@ -27,7 +26,6 @@ class Team:
         for member in self.members:
             if not any(person.id == member.id for person in value.members):
                 check = False
-        #check = set(self.members.name).issubset(value.members.name)
 
         return check
 
@@ -50,7 +48,7 @@ def make_team(teamnum, people, unitnum):
         without_person = people.copy()
         without_person.pop(personi)
         # check for what overlap there is before adding the people to the partners list
-        newteam.overlap += len(set(people[personi].partners) & set(people))/2
+        newteam.overlap += len(set(people[personi].partners) & set(people))
 
         for notperson in without_person:
             people[personi].partners.append(notperson)
@@ -65,73 +63,12 @@ def make_team(teamnum, people, unitnum):
     # that should update all the people
     return newteam
 
-
-def round_robin(teamsize,teamcount, all_people):
-    # initial setup
-    og_all_people = all_people
-    #numval = teamsize * teamcount
-    # if we need empty seats, create empty people
-    #for i in range(numval - len(all_people)):
-     #   og_all_people.append(Person("empty", -random.random(), [], []))
-    # 4 x count grid, each column is a team
-    team_grid = [[0 for x in range(teamsize)] for y in range(teamcount)]
-    round = 0
-    all_teams = []
-    first_team = [1]
-    check = False
-
-    while check is False:
-        people_list = og_all_people[:]
-        people_ind = 0
-        end = people_list[len(people_list) - round:len(people_list)]
-        # each set of teams gets a total unhappiness score, 
-        # and a check for if they're breaking the rules
-        team_set = []
-        rule_broken = False
-        total_unhappiness = 0
-        total_overlap = 0
-        if len(end) != 0:
-            people_list[1:1] = end
-        # For each round, change the team setup
-        for j in range(teamsize):
-            odd = j % 2
-            for i in range(teamcount):
-                if people_ind < len(people_list):
-                    if odd == 0:
-                        team_grid[i][j] = people_list[people_ind]
-                    # assign backwards for proper round robin
-                    elif odd == 1:
-                        team_grid[teamcount - 1 - i][j] = people_list[people_ind]
-                    people_ind = people_ind + 1
-        # Then create teams based on each column
-        for i in range(teamcount):
-            team = make_team(i,team_grid[i],round)
-            total_unhappiness += team.unhappiness
-            total_overlap += team.overlap
-            if team.rule_break:
-                rule_broken = True
-            # If this is the first team, keep it separate so we know when we go back to it
-            if i == 0 and round == 0:
-                first_team = copy.deepcopy(team)
-            # if we hit that team stop
-            else:
-                check = (team == first_team)
-            if check is True:
-                break
-            team_set.append(team)
-        # Increment rounds 
-        round = round + 1
-        # Any formation that requires a rule to be broken doesn't get considered
-        if not rule_broken and len(team_set) != 0:
-            all_teams.append([copy.deepcopy(team_set),total_unhappiness,total_overlap])
-    return all_teams
-
 def divide_chunks(l, n):
     # looping till length l
     for i in range(0, len(l), n): 
         yield l[i:i + n]
 
-def brute_force(iterations, teamsize, teamcount, all_people):
+def random_semester(iterations, teamsize, all_people):
     new_all_people = copy.deepcopy(all_people)
     it_teams = []
     it_unhappy = 0
@@ -160,7 +97,12 @@ def brute_force(iterations, teamsize, teamcount, all_people):
 
 
 def main():
-    # do main things
+    # maximum people per team
+    teamsize = 4
+    # number of units
+    units = 4
+
+    # people format: "name", id, [previous partners], [("disliked name", 0 or 1)]
     person1 = Person("person1", 0, [], [("person7",0)])
     person2 = Person("person2", 1, [], [("person8",1)])
     person3 = Person("person3", 2, [], [])
@@ -192,9 +134,7 @@ def main():
                   person11,person12,person13,person14,person15,person16,person17,person18,person19,
                   person20,person21,person22,person23,person24,person25,person26]
 
-    teamsize = 4
-    teamcount = 7
-    numval = teamsize * teamcount
+    numval = math.ceil(len(all_people) / teamsize) * teamsize
     empties = []
     # if we need empty seats, create empty people
     for i in range(numval - len(all_people)):
@@ -203,37 +143,17 @@ def main():
         all_people.append(Person(name, -random.random(), [], copy.deepcopy(empties)))
         empties.append((name,0))
 
-    round_robin_people = copy.deepcopy(all_people)
-    random_people = copy.deepcopy(all_people)
-
-    team1 = make_team(1, [person1, person2, person3], 1)
-    print(team1.get_teamstr())
-
-
-    # give the number of teams & people to assign
-    all_teams = round_robin(teamsize,teamcount,round_robin_people)
-    # sort by overlap ascending then by badness ascending
-    all_teams.sort(key=lambda x: (x[2],x[1]))
-
-    # Print out the teams and however many we can have
-    round = 0
-    for teamset in all_teams:
-        round += 1
-        r_string = "Round: " + str(round) + ", Overlap: " + str(teamset[2]) + ", Unhappiness: " + str(teamset[1])
-        print(r_string)
-        for team in teamset[0]:
-            print(team.get_teamstr())
-
     random_teams = []
     
     for i in range(100000):
-        random_team = brute_force(4,teamsize,teamcount,random_people)
+        random_team = random_semester(units,teamsize,all_people)
         random_teams.append(random_team)
+    # sort by overlap then by unhappiness
     random_teams.sort(key=lambda x: (x[2],x[1]))
 
-    # BRUTE FORCE
+    # print out the info nicely
     print("Total Overlap: " + str(random_teams[0][2]) + ", Unhappiness: " + str(random_teams[0][1]))
-    un = 0
+    un = 1
     for team_set in random_teams[0][0]:
         u_string = "Unit: " + str(un)
         print(u_string)
